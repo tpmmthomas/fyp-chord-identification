c5b35d62bbffa4a05b1220079ea0ab8aaf51e285
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2021-07-07 10:49:43 +0800

    Update chordToNote.py

--- modules/chordToNote.py
+++ modules/chordToNote.py
@@ -1,1 +1,31 @@
-print("Hi!")
+
+#0 = C, 12 = B pitch class
+major_offset = [0,4,7]
+minor_offset = [0,3,7]
+diminished_offset = [0,3,6]
+augmented_offset = [0,4,8]
+#Using chart in shared file as reference
+major_key_Chordtype = [0,1,1,0,0,1,2]
+minor_key_Chordtype = [1,2,0,1,1,0,2]
+
+key = "Cmajor"
+chord = "I"
+
+#Determines whether the chord type (major,minor,etc)
+def typeAnalysis(key,chord):
+    isSeven = chord[-1:] == "7"
+    if len(chord) >=6 and chord[0:6].upper() == "GERMAN":
+        return "ger",True
+    elif len(chord) >=6 and chord[0:6].upper() == "FRENCH":
+        return "fre",True
+    elif len(chord) >=7 and chord[0:7].upper() == "ITALIAN":
+        return "ita",True
+    elif len(chord)>=3 and chord[0:3].upper() == "DIM":
+        return "dim",True
+    elif len(chord)>=3 and chord[0:3].upper() == "AUG":
+        return "aug",True
+    elif chord[0] == "B":
+        return "maj"
+    
+def startPosition():
+
