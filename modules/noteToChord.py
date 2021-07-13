73df617e68293bfc02e7fceb765713d1c24e04b6
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2021-07-13 23:35:15 +0800

    Update noteToChord.py

--- modules/noteToChord.py
+++ modules/noteToChord.py
@@ -32,26 +32,25 @@ key_mapping={
     'B':11
 }
 
-#1 by 1 key to number
-def key2num(key):  
-  key=key.upper()
-  num=key_mapping[key[0]]
-  modifier=len(key)
-  if modifier==1:
-    return num
-  elif key[1]=='#':
-    return (num+(modifier-1))%12
-  elif key[1]=='B':
-    return (num-(modifier-1))%12
-  elif key[1]=='X':
-    return (num+(modifier-1)*2)%12
-
 # key_list to number_list
 def keys2num(keys):
-  if keys[-1]=='-':
-    return sorted([key2num(key) for key in keys[:-1]])
-  else:
-    return sorted([key2num(key) for key in keys])
+    #1 by 1 key to number
+    def key2num(key):  
+        key=key.upper()
+        num=key_mapping[key[0]]
+        modifier=len(key)
+        if modifier==1:
+            return num
+        elif key[1]=='#':
+            return (num+(modifier-1))%12
+        elif key[1]=='B':
+            return (num-(modifier-1))%12
+        elif key[1]=='X':
+            return (num+(modifier-1)*2)%12
+    if keys[-1]=='-':
+        return sorted([key2num(key) for key in keys[:-1]])
+    else:
+        return sorted([key2num(key) for key in keys])
 
 def NoteToChord(input_name,numOut=10):
     chords = []
