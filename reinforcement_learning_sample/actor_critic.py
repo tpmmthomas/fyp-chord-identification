3f7436a7ee1cddb5dbd4ed1273e752da3bf29719
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2021-09-24 14:58:53 +0800

    update RL example

--- reinforcement_learning_sample/actor_critic.py
+++ reinforcement_learning_sample/actor_critic.py
@@ -1,28 +1,29 @@
 import gym
 import numpy as np
-from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Input
-from keras.layers.merge import Add, Multiply
-from keras.optimizers import Adam
-import keras.backend as K
-
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Dropout, Input
+from tensorflow.keras.layers import Add, Multiply
+from tensorflow.keras.optimizers import Adam
+import tensorflow.keras.backend as K
 import tensorflow as tf
-
+import pandas as pd
+from env import CryptoSingleEnv
 import random
 from collections import deque
+import time
 
+# OR SIMPLY USE basemark
 # determines how to assign values to each state, i.e. takes the state
 # and action (two-input model) and determines the corresponding value
 class ActorCritic:
-    def __init__(self, env, sess):
+    def __init__(self, env):
         self.env = env
-        self.sess = sess
 
         self.learning_rate = 0.001
-        self.epsilon = 1.0
-        self.epsilon_decay = 0.995
-        self.gamma = 0.95
-        self.tau = 0.125
+        self.epsilon = 1.0  # explore or exploit probability
+        self.epsilon_decay = 0.9999  # gradually lean towards exploitation
+        self.gamma = 0.95  # Future rewards discount rate
+        self.tau = 0.125  # Prob of updating the target model
 
         # ===================================================================== #
         #                               Actor Model                             #
@@ -35,19 +36,21 @@ class ActorCritic:
         self.actor_state_input, self.actor_model = self.create_actor_model()
         _, self.target_actor_model = self.create_actor_model()
 
-        self.actor_critic_grad = tf.placeholder(
-            tf.float32, [None, self.env.action_space.shape[0]]
-        )  # where we will feed de/dC (from critic)
+        # self.actor_critic_grad = tf.placeholder(
+        #     tf.float32, [None, self.env.action_space.shape[0]]
+        # )  # where we will feed de/dC (from critic)
 
-        actor_model_weights = self.actor_model.trainable_weights
-        self.actor_grads = tf.gradients(
-            self.actor_model.output, actor_model_weights, -self.actor_critic_grad
-        )  # dC/dA (from actor)
-        grads = zip(self.actor_grads, actor_model_weights)
-        self.optimize = tf.train.AdamOptimizer(self.learning_rate).apply_gradients(
-            grads
+        # actor_model_weights = self.actor_model.trainable_weights
+        # self.actor_grads = tf.gradients(
+        #     self.actor_model.output, actor_model_weights, -self.actor_critic_grad
+        # )  # dC/dA (from actor)
+        # grads = zip(self.actor_grads, actor_model_weights)
+        # self.optimize = tf.train.AdamOptimizer(self.learning_rate).apply_gradients(
+        #     grads
+        # )
+        self.actor_optimizer = tf.keras.optimizers.Adam(
+            learning_rate=self.learning_rate
         )
-
         # ===================================================================== #
         #                              Critic Model                             #
         # ===================================================================== #
@@ -59,25 +62,34 @@ class ActorCritic:
         ) = self.create_critic_model()
         _, _, self.target_critic_model = self.create_critic_model()
 
-        self.critic_grads = tf.gradients(
-            self.critic_model.output, self.critic_action_input
-        )  # where we calcaulte de/dC for feeding above
+        # self.critic_grads = tf.gradients(
+        #     self.critic_model.output, self.critic_action_input
+        # )  # where we calcaulte de/dC for feeding above
 
-        # Initialize for later gradient calculations
-        self.sess.run(tf.initialize_all_variables())
+        # # Initialize for later gradient calculations
+        # self.sess.run(tf.initialize_all_variables())
 
     # ========================================================================= #
     #                              Model Definitions                            #
     # ========================================================================= #
 
     def create_actor_model(self):
+        def mapping_to_target_range(x, target_min=0, target_max=3):
+            x02 = K.tanh(x) + 1  # x in range(0,2)
+            scale = (target_max - target_min) / 2.0
+            final = x02 * scale + target_min
+            todivide = tf.constant([1, 3, 1, 3], dtype=tf.float32)
+            return tf.divide(final, todivide)
+
+        f = tf.function(mapping_to_target_range)
         state_input = Input(shape=self.env.observation_space.shape)
         h1 = Dense(24, activation="relu")(state_input)
         h2 = Dense(48, activation="relu")(h1)
-        h3 = Dense(24, activation="relu")(h2)
-        output = Dense(self.env.action_space.shape[0], activation="relu")(h3)
+        # just added regularizer, see if still have nan
+        h3 = Dense(24, activation="relu", kernel_regularizer="l2")(h2)
+        output = Dense(self.env.action_space.shape[0], activation=f)(h3)
 
-        model = Model(input=state_input, output=output)
+        model = Model(inputs=state_input, outputs=output)
         adam = Adam(lr=0.001)
         model.compile(loss="mse", optimizer=adam)
         return state_input, model
@@ -91,9 +103,9 @@ class ActorCritic:
         action_h1 = Dense(48)(action_input)
 
         merged = Add()([state_h2, action_h1])
-        merged_h1 = Dense(24, activation="relu")(merged)
-        output = Dense(1, activation="relu")(merged_h1)
-        model = Model(input=[state_input, action_input], output=output)
+        merged_h1 = Dense(24, activation="relu", kernel_regularizer="l2")(merged)
+        output = Dense(1, activation="sigmoid")(merged_h1)
+        model = Model(inputs=[state_input, action_input], outputs=output)
 
         adam = Adam(lr=0.001)
         model.compile(loss="mse", optimizer=adam)
@@ -109,22 +121,33 @@ class ActorCritic:
     def _train_actor(self, samples):
         for sample in samples:
             cur_state, action, reward, new_state, _ = sample
-            predicted_action = self.actor_model.predict(cur_state)
-            grads = self.sess.run(
-                self.critic_grads,
-                feed_dict={
-                    self.critic_state_input: cur_state,
-                    self.critic_action_input: predicted_action,
-                },
-            )[0]
+            cur_state = np.array(cur_state)
+            cur_state = tf.constant(cur_state)
+            # need change to target model?
+            with tf.GradientTape() as tape:
+                tape.watch(cur_state)
+                predicted_action = self.actor_model(cur_state) #dC/dA
+                predicted_reward = self.critic_model([cur_state, predicted_action]) # de/dC
+            actor_model_weights = self.actor_model.trainable_weights
+            dEdA = tape.gradient(predicted_reward, actor_model_weights)
 
-            self.sess.run(
-                self.optimize,
-                feed_dict={
-                    self.actor_state_input: cur_state,
-                    self.actor_critic_grad: grads,
-                },
-            )
+            # tf.cast(dEdA, tf.float32)
+            # dEdA = tf.gradients(predicted_action, actor_model_weights, -dEdC)
+            self.actor_optimizer.apply_gradients(zip(dEdA, actor_model_weights))
+            # grads = self.sess.run(
+            #     self.critic_grads,
+            #     feed_dict={
+            #         self.critic_state_input: cur_state,
+            #         self.critic_action_input: predicted_action,
+            #     },
+            # )[0]
+            # self.sess.run(
+            #     self.optimize,
+            #     feed_dict={
+            #         self.actor_state_input: cur_state,
+            #         self.actor_critic_grad: grads,
+            #     },
+            # )
 
     def _train_critic(self, samples):
         for sample in samples:
@@ -135,17 +158,26 @@ class ActorCritic:
                     [new_state, target_action]
                 )[0][0]
                 reward += self.gamma * future_reward
-            self.critic_model.fit([cur_state, action], reward, verbose=0)
+            # print(cur_state, action, reward)
+            # cur_state = np.array(cur_state)
+            # action = np.array(action)
+            reward = np.array([reward])
+            # cur_state = cur_state.astype(np.float64)
+            # print(cur_state, action, reward)
+            self.critic_model.fit([cur_state, action], [reward], verbose=0)
 
     def train(self):
         batch_size = 32
         if len(self.memory) < batch_size:
             return
-
-        rewards = []
         samples = random.sample(self.memory, batch_size)
+        # print(samples)
+        # print("training critic")
         self._train_critic(samples)
+        # print("training actor")
         self._train_actor(samples)
+        if random.random() < self.tau:  # Updating the target model here
+            self.update_target()
 
     # ========================================================================= #
     #                         Target Model Updating                             #
@@ -153,19 +185,19 @@ class ActorCritic:
 
     def _update_actor_target(self):
         actor_model_weights = self.actor_model.get_weights()
-        actor_target_weights = self.target_critic_model.get_weights()
+        actor_target_weights = self.target_actor_model.get_weights()
 
         for i in range(len(actor_target_weights)):
             actor_target_weights[i] = actor_model_weights[i]
-        self.target_critic_model.set_weights(actor_target_weights)
+        self.target_actor_model.set_weights(actor_target_weights)
 
     def _update_critic_target(self):
         critic_model_weights = self.critic_model.get_weights()
-        critic_target_weights = self.critic_target_model.get_weights()
+        critic_target_weights = self.target_critic_model.get_weights()
 
         for i in range(len(critic_target_weights)):
             critic_target_weights[i] = critic_model_weights[i]
-        self.critic_target_model.set_weights(critic_target_weights)
+        self.target_critic_model.set_weights(critic_target_weights)
 
     def update_target(self):
         self._update_actor_target()
@@ -179,33 +211,64 @@ class ActorCritic:
         self.epsilon *= self.epsilon_decay
         if np.random.random() < self.epsilon:
             return self.env.action_space.sample()
-        return self.actor_model.predict(cur_state)
+        else:
+            print("Predicting")
+        return self.actor_model.predict(
+            cur_state
+        )  # This is using the training model not the target model
+
+    # ========================================================================= #
+    #                              Model Validations                            #
+    # ========================================================================= #
+    def validation(self, cur_state):
+        return self.target_actor_model.predict(cur_state)
+
+    # ========================================================================= #
+    #                              Model Checkpoint                             #
+    # ========================================================================= #
+
+    def save_model(self, version):
+        self.target_actor_model.save("./saved_model/actor" + version + "/")
+        self.target_critic_model.save("./saved_model/critic" + version + "/")
+        return
+
+    def reload_model(self, actor_weights, critic_weights):
+        self.actor_model.set_weights(actor_weights)
+        self.target_actor_model
+        pass
 
 
 def main():
-    sess = tf.Session()
-    K.set_session(sess)
-    env = gym.make("Pendulum-v0")
-    actor_critic = ActorCritic(env, sess)
+    dfnorm = pd.read_csv("training_data/BTCUSDT_5min_norm.csv")
+    dfnorm = dfnorm.iloc[:, 1:]
+    df = pd.read_csv("training_data/BTCUSDT_5min.csv")
+    df = df.iloc[:, 1:]
+    print(df.head(6))
+    env = CryptoSingleEnv(df, dfnorm)
+    actor_critic = ActorCritic(env)
 
-    num_trials = 10000
-    trial_len = 500
+    num_trials = 2
 
     cur_state = env.reset()
     action = env.action_space.sample()
-    while True:
-        env.render()
-        cur_state = cur_state.reshape((1, env.observation_space.shape[0]))
-        action = actor_critic.act(cur_state)
-        action = action.reshape((1, env.action_space.shape[0]))
+    for i in range(num_trials):
+        done = False
+        while not done:
+            env.render()
+            cur_state = cur_state.reshape((1, env.observation_space.shape[0]))
+            action = actor_critic.act(cur_state)
+            action = action.reshape((1, env.action_space.shape[0]))
 
-        new_state, reward, done, _ = env.step(action)
-        new_state = new_state.reshape((1, env.observation_space.shape[0]))
+            print("Action taken:", action)
+            new_state, reward, done, _ = env.step(action)
+            new_state = new_state.reshape((1, env.observation_space.shape[0]))
 
-        actor_critic.remember(cur_state, action, reward, new_state, done)
-        actor_critic.train()
+            actor_critic.remember(cur_state, action, reward, new_state, done)
+            actor_critic.train()
 
-        cur_state = new_state
+            cur_state = new_state
+            # time.sleep(0.1)
+        cur_state = env.reset()
 
 
 if __name__ == "__main__":
--- reinforcement_learning_sample/env.py
+++ reinforcement_learning_sample/env.py
@@ -2,6 +2,7 @@ from gym import Env
 from gym.spaces import Discrete, Box
 import numpy as np
 import math
+from datetime import datetime
 
 
 def round_up(n, decimals=0):
@@ -15,40 +16,85 @@ def round_down(n, decimals=0):
 
 
 class CryptoSingleEnv(Env):  # Fit one particular coin first
-    def __init__(self, df):
+    def __init__(self, df, dfnorm):
         self.leverage = 2
+        # Number of k-line data before current time given as observation state
+        self.look_back_window = 12
+        """
+        To append state:
+        y = x.copy()
+        for i in range(self.look_back_window-1):
+                y = np.vstack((y,x))
+        """
         # Training / Validation dataset
         self.df = df
+        self.dfnorm = dfnorm
         self.dp_coin = 3
-        self.dp_usdt = 8
+        self.dp_usdt = 3
         # Actions we can take: buy long (0-100%), sell long(0-100%), hold long (), buy short (0-100%), sell short(0-100%), hold short()
         # Dimensions are defined as: [Long action(0:buy,1:sell,2:hold), Long percentage(0-100%), short action, short percentage].
         # Percentage is defined as: amount of available balance (when buy), will multiply with leverage afterwards. Amount of total opened positions (when sell). Ignore when hold.
         self.action_space = Box(
-            low=np.array([[0, 0, 0, 0]]), high=[3, 1, 3, 1], dtype=np.float16
+            low=np.array([0, 0, 0, 0]), high=np.array([3, 1, 3, 1]), dtype=np.float16
         )
         # [Open,High,Low,Close,Volume (USDT),tradecount,fundingrate,...(append balancestate)...] (add RSI, MACD, multiple tokens later?)
         self.observation_space = Box(
-            low=np.array([0, 0, 0, 0, 0, 0, -1]),  # ,0,0,0,0,0,0,0,0,np.NINF]),
+            low=np.array(
+                [
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    -1,
+                    0,
+                    -1,
+                    0,
+                    0,
+                    0,
+                    0,
+                ]
+            ),
             high=np.array(
-                [np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, 1]
-            ),  # np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf, np.inf]),
+                [
+                    np.inf,
+                    np.inf,
+                    np.inf,
+                    np.inf,
+                    np.inf,
+                    np.inf,
+                    1,
+                    np.inf,
+                    np.inf,
+                    np.inf,
+                    np.inf,
+                ]
+            ),
         )
         # [Total Balance, Available balance, Long position (amt), long margin, long avg entry price, Short position (amt), short margin,short avg entry, sharpe_ratio]
-        self.state = np.array([10000, 10000, 0, 0, 0, 0, 0, 0, 0])
+        self.state = np.array([10000, 10000, 0, 0, 0, 0, 0, 0, 0], dtype=np.float32)
         self.timestamp = df["timestamp"].values[0]
         self.endtime = df["timestamp"].values[-1]
         self.pastbal = [10000]
         self.pastroi = []
         self.starttime = self.timestamp
+        self.latest_price = 0
 
     def step(self, action):
+        action = action[0]
         # check liquidate condition and update balance
-        self.timestamp += 60000  # 1 minute currently
-        latest_data = self.df.loc[self.df["timestamp"] == self.timestamp].values
+        # print("State checking", self.state)
+        latest_data = []
+        while len(latest_data) == 0:
+            self.timestamp += 60000  # 1 minute currently
+            latest_data = self.df.loc[self.df["timestamp"] == self.timestamp].values
+        latest_data = latest_data[0]
+        latest_data2 = self.dfnorm.loc[self.df["timestamp"] == self.timestamp].values[0]
+        # timestamp,open,high,low,close,volume,funding_rate,funding_time
         funding_time = latest_data[7]
         funding_rate = latest_data[6]
-        open_price = latest_data[0]
+        open_price = latest_data[1]
+        self.latest_price = open_price
+        # print("Latest price", open_price)
         pnl_long = (open_price - self.state[4]) * self.state[2]
         if pnl_long < 0 and self.state[3] + pnl_long <= 0:
             self.state[3] = 0
@@ -62,33 +108,39 @@ class CryptoSingleEnv(Env):  # Fit one particular coin first
         self.state[0] = (
             self.state[1] + self.state[3] + self.state[6] + pnl_long + pnl_short
         )
-        if funding_time == self.timestamp:  # It's funding time!
+        # print(f"state {self.state} pnl_long : {pnl_long},pnl_short: {pnl_short}")
+        if self.state[0] <= 0:
+            done = True
+        if (
+            funding_time <= self.timestamp and funding_time > self.timestamp - 60000
+        ):  # It's funding time!
+            # print("Funding right now!")
             net_position = self.state[2] - self.state[5]
             funding = net_position * open_price * funding_rate
             self.state[1] -= funding
             self.state[0] -= funding
         # Apply action
         if action[0] // 1 == 0:  # Buy long
-            if action[1] >= 98:
-                action[1] = 100
-            elif action[1] <= 2:
+            if action[1] >= 0.98:
+                action[1] = 1
+            elif action[1] <= 0.02:
                 action[1] = 0
             amount_to_buy = min(
-                self.state[1],
+                self.state[1] * self.leverage,
                 round_up(self.state[1] * action[1] * self.leverage, self.dp_usdt),
             )  # in USDT
             amount_coin = round_down((amount_to_buy / open_price), self.dp_coin)
             if amount_coin > 0:
                 self.state[1] -= amount_to_buy / self.leverage
                 self.state[4] = (self.state[4] * self.state[2] + amount_to_buy) / (
-                    self.state[2] + amount_coin
+                    self.state[2] + amount_to_buy / open_price
                 )
                 self.state[2] += amount_coin
                 self.state[3] += amount_to_buy / self.leverage
         elif action[0] // 1 == 1:  # sELL LONG
-            if action[1] >= 98:
-                action[1] = 100
-            elif action[1] <= 2:
+            if action[1] >= 0.98:
+                action[1] = 1
+            elif action[1] <= 0.02:
                 action[1] = 0
             amount_to_sell = min(
                 self.state[2], round_up(self.state[2] * action[1], self.dp_coin)
@@ -100,27 +152,29 @@ class CryptoSingleEnv(Env):  # Fit one particular coin first
                 self.state[3] = max(0, self.state[3])
                 self.state[2] -= amount_to_sell
                 self.state[1] += return_leverage + pnl
+            if self.state[2] == 0:
+                self.state[4] = 0
         if action[2] // 1 == 0:  # SHORT Sell
-            if action[3] >= 98:
-                action[3] = 100
-            elif action[3] <= 2:
+            if action[3] >= 0.98:
+                action[3] = 1
+            elif action[3] <= 0.02:
                 action[3] = 0
             amount_to_buy = min(
-                self.state[1],
+                self.state[1] * self.leverage,
                 round_up(self.state[1] * action[3] * self.leverage, self.dp_usdt),
             )  # in USDT
-            amount_coin = round_down((amount_to_buy / open_price), self.dp_coin)
+            amount_coin = round_up((amount_to_buy / open_price), self.dp_coin)
             if amount_coin > 0:
                 self.state[1] -= amount_to_buy / self.leverage
                 self.state[7] = (self.state[7] * self.state[5] + amount_to_buy) / (
-                    self.state[5] + amount_coin
+                    self.state[5] + amount_to_buy / open_price
                 )
                 self.state[5] += amount_coin
                 self.state[6] += amount_to_buy / self.leverage
         elif action[2] // 1 == 1:  # short buy
-            if action[3] >= 98:
-                action[3] = 100
-            elif action[3] <= 2:
+            if action[3] >= 0.98:
+                action[3] = 1
+            elif action[3] <= 0.02:
                 action[3] = 0
             amount_to_sell = min(
                 self.state[5], round_up(self.state[5] * action[3], self.dp_coin)
@@ -132,23 +186,30 @@ class CryptoSingleEnv(Env):  # Fit one particular coin first
                 self.state[6] = max(0, self.state[6])
                 self.state[5] -= amount_to_sell
                 self.state[1] += return_leverage + pnl
+            if self.state[5] == 0:
+                self.state[7] = 0
         else:
             pass
 
         # # Calculate reward
         # daily roi% + current time multiplied by weight, then calculate mean and variance
-        latest_roi = (self.state[0] - self.pastbal[-1]) / self.pastbal[-1]
+        # latest_roi = (self.state[0] - self.pastbal[-1]) / self.pastbal[-1]
         sharpe_roi = self.pastroi.copy()
-        sharpe_roi.append(latest_roi)
-        reward = np.mean(sharpe_roi) / np.std(sharpe_roi)  # sharpe ratio
+        # sharpe_roi.append(latest_roi)
+        # print(sharpe_roi)
+        if len(sharpe_roi) == 1:
+            reward = sharpe_roi[0]
+        else:
+            reward = np.mean(sharpe_roi) / np.std(sharpe_roi)  # sharpe ratio
+            reward = 0 if np.isnan(reward) or np.isinf(reward) else reward
         self.state[-1] = reward
-        if (self.timestamp - self.startime) % 86400000 == 0:
+        if (self.timestamp - self.starttime) % 86400000 == 0:
             self.pastbal.append(self.state[0])
             self.pastroi.append(
                 (self.pastbal[-1] - self.pastbal[-2]) / self.pastbal[-2]
             )
         # Check if shower is done
-        if self.timestamp == self.endtime or self.state[0] <= 0:
+        if self.timestamp == self.endtime:
             done = True
         else:
             done = False
@@ -157,22 +218,36 @@ class CryptoSingleEnv(Env):  # Fit one particular coin first
         # self.state += random.randint(-1,1)
         # Set placeholder for info
         info = {}
-
+        stateinfo = latest_data2.tolist()[1:]
+        stateinfo.extend(
+            self.state[0:3]
+        )  # TODO: consider also normalize the position amount (e.g. as a percentage to total?)
+        stateinfo.append(self.state[5])
         # Return step information
-        return self.state, reward, done, info
+        return np.array(stateinfo), reward, done, info
 
     def render(self):
-        print(f"Current balance: {self.state[0]}, Sharpe Ratio: {self.state[-1]}")
+        dto = datetime.fromtimestamp(self.timestamp // 1000)
+        x = f"Current balance: {self.state[0]}, Sharpe Ratio: {self.state[-1]}, Current Price: {self.latest_price}, time : {str(dto)}."
+        y = f"Long position amt: {self.state[2]}, avg entry price: {self.state[4]}, short position amt: {self.state[5]}, avg entry price: {self.state[7]}"
+        print(x)
+        print(y)
         return
 
     def reset(self, df=None):
-        self.state = np.array([10000, 10000, 0, 0, 0, 0, 0])
+        self.state = np.array([10000, 10000, 0, 0, 0, 0, 0, 0, 0], dtype=np.float32)
         self.pastbal = [10000]
         self.pastroi = []
         if not df is None:
             self.df = df
-        self.timestamp = df["timestamp"].values[0]
-        return self.state
+        self.timestamp = self.df["timestamp"].values[0]
+        self.starttime = self.timestamp
+        self.endtime = self.df["timestamp"].values[-1]
+        latest_data = self.df.loc[self.df["timestamp"] == self.timestamp].values
+        stateinfo = latest_data.tolist()[0][1:]
+        stateinfo.extend(self.state[0:3])
+        stateinfo.append(self.state[5])
+        return np.array(stateinfo)
 
 
 """
